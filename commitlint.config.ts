88d774632e954aa36bae9008fc8ef9e427a5ee7a
Jairus 'JJ' Joer <jairus.joer@bwi.de>
2026-09-17 18:15:08 +0200

    chore(repo): align configurations for cohesive experience

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,8 +1,13 @@
+import { readdirSync } from 'node:fs';
 import { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
+
+const apps = readdirSync('apps');
+const packages = readdirSync('packages').filter((pkg) => !['.gitkeep'].includes(pkg));
 
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'scope-enum': [2, 'always', ['repo', ...apps, ...packages]],
     'scope-empty': [RuleConfigSeverity.Error, 'never'],
   },
 } satisfies UserConfig;
